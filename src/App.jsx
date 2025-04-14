158e4af91272613769c46259a69931e0469fcb10
gongbu22 <pyujin0711@naver.com>
2025-04-14 23:28:26 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h2 className='text-blue-500 text-xl font-bold'>Hello, World!</h2>
+      <p className='text-lg font-medium'> Hello yujin's portfolio</p>
+    </div>
+  )
+}
+
+export default App

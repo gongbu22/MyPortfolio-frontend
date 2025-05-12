afd17af1d447512461ebd8d444968ce1c69ae730
gongbu22 <pyujin0711@naver.com>
2025-05-13 00:52:27 +0900

    수정

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server: {
     host: '0.0.0.0',
+    allowedHosts: [
+      'yujinfolio.com', 
+      'localhost',   
+      '127.0.0.1'     
+    ],
     proxy: {
       '/ws': {
         target: 'ws://front-nodejs-svc:5173',  // Kubernetes 내 서비스 이름
